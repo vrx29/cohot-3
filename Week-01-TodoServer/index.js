462ff773aac2b1eaa5d2eafeae5995a9b5c55c35
vineet <jammackp@gmail.com>
2026-02-16 19:54:00 +0530

    week 1 done

---
+++ Week-01-TodoServer/index.js
@@ -0,0 +1,73 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+let itemId = 1;
+const todos = [
+  {
+    id: itemId,
+    title: "Test Todd",
+    description: "This is a basic description",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("This is a todo server");
+});
+
+//GET
+app.get("/todos", (req, res) => {
+  res.status(200).json({
+    todos,
+  });
+});
+
+//GET /todos/:id
+app.get("/todos/:id", (req, res) => {
+  const { id } = req.params;
+
+  const todo = todos.find((item) => item.id == id);
+  if (!todo) {
+    res.status(404).send();
+  }
+  res.status(200).json(todo);
+});
+
+//POST /todos
+app.post("/todos", (req, res) => {
+  const newTodo = { id: ++itemId, title: req.body.title, description: req.body.description }
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put("/todos/:id", (req,res)=>{
+    const todoIndex = todos.findIndex(item => item.id == parseInt(req.params.id));
+    if(todoIndex == -1){
+        res.status(404).send();
+    }
+    console.log(todos)
+    console.log(todoIndex)
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    res.status(200).json(todos[todoIndex])
+})
+
+app.delete("/todos/:id", (req, res)=>{
+     const todoIndex = todos.findIndex(item => item.id == parseInt(req.params.id));
+    if(todoIndex == -1){
+        res.status(404).send();
+    }
+    todos.splice(todoIndex,1)
+    res.status(200).send() 
+})
+
+  app.use((req, res, next) => {
+    res.status(404).send();
+  });
+
+app.listen(3000, () => console.log("Server started on port 3000"));
+
+
+
---
+++ Week-01/01-node.js
@@ -0,0 +1,39 @@
+// ECMA Script
+// ------------
+// It  is a Javascript specification on which JS is based.
+
+
+// Javascript
+// Scripting language that is based on ECMA specification. it mostly widely used specification of ECMA script
+// Beyond ECMA script JS also includes additional features which are not part of ECMA script spec like DOM, and web apis.
+// Common JS engines
+// V8 - Chrome and Spider Monkey - Mozilla
+
+
+// Node JS
+// ------------------------------
+// some people took out V8 engine and added some backend things like (filesystem reads) on top to create new runtime to make it support backend.
+
+
+// ***************************************************************************
+
+
+// HTTP Server 
+// ----------
+// A protocol defined for machines to communicate. Specifically for websites.
+// HTTP PROTOCOL
+/*
+// CLient Side
+----------------
+1. Protocol(HTTP/HTTPS)
+2. Address(URL/IP/PORT)
+3. Route
+4. Headers, Body, Query params
+5. Methods (GET. PUT, POST, DELETE)
+
+// Server Side
+-----------------
+1. Response headers
+2. Response body
+3. Status Code(404, 200. 403)
+*/
