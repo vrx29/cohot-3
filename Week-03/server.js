5ab3b771758463414f7b59e95a7ea5fa2d9604c5
vineet <jammackp@gmail.com>
2026-03-09 22:55:57 +0530

    added info

--- Week-03/server.js
+++ Week-03/server.js
@@ -21,24 +21,48 @@ app.use(function(err, req, res, next){
 //
 */
 
-
 // Authentication
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/user-health', (req, res)=>{
-    res.json({
-        msg: 'User is healthy'
-    })
-})
+app.get("/user-health", (req, res) => {
+  res.json({
+    msg: "User is healthy",
+  });
+});
 
-app.listen(3000, ()=> console.log('Server started on port : 3000'))
-
-
+app.listen(3000, () => console.log("Server started on port : 3000"));
 
 // Authentication
- 
+// 3 Types
+// 1. Hashing
+/*
+Hashing converts data into a fixed-length string using a one-way function.
+Input → password
+Output → random-looking string called hash
+
+Password: mypassword123
+Hash:
+9b74c9897bac770ffc029102a200c5de
+
+Its one way we cannot convert hash back to original data. Used for storing passwords in hash.
+
+-----------------------------------------
+2. Encryption
+converts data into unreadable form but can be reversed
+Plain Text -> Encryption -> Cipher Text
+Cipher Text -> Decryption -> Plain Text
+
+Its two way and requires key to decrypt.
+used in RSA, HTTPS, TLS
+
+------------------------------------------
+3. JWT
+HEADER.PAYLOAD.SIGNATURE
+
+
+*/
